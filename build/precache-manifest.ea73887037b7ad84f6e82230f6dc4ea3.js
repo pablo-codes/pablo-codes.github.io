57d5f393c65575f9bddd5733ba3e56bb6c1f7d6c
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-07-05 15:18:51 +0100

    first commit

---
+++ build/precache-manifest.ea73887037b7ad84f6e82230f6dc4ea3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "183cf6f514e2190dad1f",
+    "url": "/static/css/main.68d0737e.chunk.css"
+  },
+  {
+    "revision": "183cf6f514e2190dad1f",
+    "url": "/static/js/main.183cf6f5.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "f89b37e964a32e7095bb",
+    "url": "/static/js/2.f89b37e9.chunk.js"
+  },
+  {
+    "revision": "7f2d4e2d2fa34e27db68",
+    "url": "/static/js/runtime~main.7f2d4e2d.js"
+  },
+  {
+    "revision": "82ece44f04e3639be2865d157d9c80b4",
+    "url": "/static/media/1.82ece44f.jpg"
+  },
+  {
+    "revision": "8dc3f0ebb5d504cb5e25ac6ea0031fbf",
+    "url": "/static/media/4.8dc3f0eb.jpg"
+  },
+  {
+    "revision": "4d071d77599e80fc0c320ce29c65bb1b",
+    "url": "/index.html"
+  }
+];

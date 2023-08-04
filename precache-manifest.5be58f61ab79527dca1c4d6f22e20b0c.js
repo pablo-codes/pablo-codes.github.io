79cc1fb6a0dbf588bc9d6a0a6c136b1d8e16efd5
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-08-04 12:42:50 +0100

    Updates

---
+++ precache-manifest.5be58f61ab79527dca1c4d6f22e20b0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0d289342fc76964626f",
+    "url": "/static/css/main.c5bfb150.chunk.css"
+  },
+  {
+    "revision": "a0d289342fc76964626f",
+    "url": "/static/js/main.a0d28934.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "9f3e76fa7af35cc365b4",
+    "url": "/static/js/2.9f3e76fa.chunk.js"
+  },
+  {
+    "revision": "7f2d4e2d2fa34e27db68",
+    "url": "/static/js/runtime~main.7f2d4e2d.js"
+  },
+  {
+    "revision": "82ece44f04e3639be2865d157d9c80b4",
+    "url": "/static/media/1.82ece44f.jpg"
+  },
+  {
+    "revision": "8dc3f0ebb5d504cb5e25ac6ea0031fbf",
+    "url": "/static/media/4.8dc3f0eb.jpg"
+  },
+  {
+    "revision": "d1a2c08ca1bc1b9890bf5285d42d6642",
+    "url": "/index.html"
+  }
+];
